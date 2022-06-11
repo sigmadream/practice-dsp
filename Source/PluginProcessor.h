88b15c0047e917c25932eb24fb766299cd81eb00
Sangkon, Han <sigmadream@gmail.com>
2022-06-12 04:25:43 +0900

    LowCut Params (#5)
    * LowCut 필터 작동확인
    * LowCut 필터 매개변수 전달

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,11 +10,21 @@
 
 #include <JuceHeader.h>
 
+
+enum Slope
+{
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
+
 struct ChainSettings
 {
     float peakFreq { 0 }, peakGainInDecibels{ 0 }, peakQuality {1.f};
     float lowCutFreq { 0 }, highCutFreq { 0 };
-    int lowCutSlope { 0 }, highCutSlope { 0 };
+    Slope lowCutSlope { Slope::Slope_12 }, highCutSlope { Slope::Slope_12 };
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
@@ -81,6 +91,7 @@ private:
         Peak,
         HighCut
     };
+        
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SdEQAudioProcessor)
 };
